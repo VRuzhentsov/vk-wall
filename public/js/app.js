5ffb87a15608275f37028fdabba465f4c2688b34
Vitalii Ruzhentsov <v.ruzhentsov@gmail.com>
2017-04-20 23:54:10 +0300

    Remove cached js

--- public/js/app.js
+++ public/js/app.js
@@ -32809,13 +32809,7 @@ module.exports = Component.exports
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
-  return _c('div', [_c('asideBlock', {
-    attrs: {
-      "show": true,
-      "placement": "left",
-      "header": "Users list"
-    }
-  }, [_c('sidebar')], 1), _vm._v(" "), _c('wall')], 1)
+  return _c('div', [_c('wall')], 1)
 },staticRenderFns: []}
 module.exports.render._withStripped = true
 if (false) {
--- resources/assets/js/components/AppBlock.vue
+++ resources/assets/js/components/AppBlock.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <asideBlock :show=true placement="left" header="Users list">
-            <sidebar></sidebar>
-        </asideBlock>
+        <!--<asideBlock :show=true placement="left" header="Users list">-->
+            <!--<sidebar></sidebar>-->
+        <!--</asideBlock>-->
 
         <wall></wall>
     </div>
